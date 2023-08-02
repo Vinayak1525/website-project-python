5d5b299f281460745a7b0d33246b55215bc766d4
Vinayak1525 <77600232+Vinayak1525@users.noreply.github.com>
2023-08-02 11:03:50 +0000

    adding style to home page

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 
 def hello_world():
-  return "Hello World, I am alive"
+  return render_template('home.html')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   app.run(host='0.0.0.0',debug=True)
 
 

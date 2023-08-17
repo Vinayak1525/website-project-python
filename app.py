ff67e1d88b907b99ca0ef5e1f5e86f6139dc30f3
vinayak1525 <vinayakmsurie17@gmail.com>
2023-08-17 22:48:52 +1200

    Dictionary/ displayed data from Database

--- app.py
+++ app.py
@@ -1,41 +1,33 @@
 from flask import Flask, render_template, jsonify
+from database import load_gameslist_from_db
 
 app = Flask(__name__)
 
+# HARD CODDING THE DATA
 
-home_list = [
-    {
-    'id':1,
-    'title':'Dota2',
-    'location':'Steam',
-    'cost': 'free to play'
-    },
-    {
-    'id':2,
-    'title':'Warframe',
-    'location':'Steam',
-    'cost':'free to play'
-    },
-    {
-    'id':3,
-    'title':'Fortnite',
-    'location':'Epic Games',
-    'cost':'free to play'
-    },
-    {
-    'id': 4,
-    'title':'Crusader Kings 3',
-    'location':'Steam',
-    'cost':'50$'
-    }
-]
+# home_list = [
+#     {"id": 1, "title": "Dota2", "location": "Steam", "cost": "free to play"},
+#     {"id": 2, "title": "Warframe", "location": "Steam", "cost": "free to play"},
+#     {"id": 3, "title": "Fortnite", "location": "Epic Games", "cost": "free to play"},
+#     {"id": 4, "title": "Crusader Kings 3", "location": "Steam", "cost": "50$"},
+# ]
+
+
+# HAD TO IMPORT ENGINE FROM DATABASE  #GETTING DATA FROM THE DATABASE
+
+
+# INITIAL PAGE
+
 
 @app.route("/")
 def hello_world():
-    return render_template("home.html",games=home_list,website='BaneNZ') 
+    games_data = load_gameslist_from_db()
+    return render_template("home.html", games=games_data, website="BaneNZ")
+
 
 # DIFFERENCE BETWEEN CREATING AN HTML END-POINT AND A JSON END-POINT
 # USE render_template for HTML and Jsonify for JSON
+
 
 @app.route("/api/games")
 def list_games():
@@ -44,4 +36,3 @@ def list_games():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
-
--- database.py
+++ database.py
@@ -1,41 +1,54 @@
 from sqlalchemy import create_engine, text
-db_con_string="mysql+pymysql://azure_admin:Password123!@flexi-banenz.mysql.database.azure.com/games_banenz"
+
+db_con_string = "mysql+pymysql://azure_admin:Password123!@flexi-banenz.mysql.database.azure.com/games_banenz"
 
 connection_string = (
-    'DRIVER=MySQL ODBC 8.0 ANSI Driver;'
-    'SERVER=flexi-banenz.mysql.database.azure.com;'
-    'PORT=3307;'
-    'DATABASE=games_banenz;'
-    'UID=azure_admin;'
-    'PWD=Password123!;'
-    'charset=utf8mb4;'
+    "DRIVER=MySQL ODBC 8.0 ANSI Driver;"
+    "SERVER=flexi-banenz.mysql.database.azure.com;"
+    "PORT=3307;"
+    "DATABASE=games_banenz;"
+    "UID=azure_admin;"
+    "PWD=Password123!;"
+    "charset=utf8mb4;"
 )
 
 
-#engine=create_engine("mysql+pymysql://azure_admin:Password123!@flexi-banenz.mysql.database.azure.com?charset=utf8mb4")
-
+# engine=create_engine("mysql+pymysql://azure_admin:Password123!@flexi-banenz.mysql.database.azure.com?charset=utf8mb4")
 
 
 engine = create_engine(
     db_con_string,
-    connect_args={
-        "ssl": {
-            "ssl_ca": "/var/www/html/DigiCertGlobalRootG2.crt.pem"
-        }
-    }
+    connect_args={"ssl": {"ssl_ca": "/var/www/html/DigiCertGlobalRootG2.crt.pem"}},
 )
 
 
+def load_gameslist_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from games_banenz.gameslist"))
+    key_dicts = result.keys()
+    Games_list = []
+    for row in result.all():
+        Games_list.append(dict(zip(key_dicts, row)))
+    return Games_list
 
-with engine.connect() as conn:
-    result=conn.execute(text("select * from gameslist"))
-    data1=result.all()[0] 
 
-    #store data into a dictionary
+# with engine.connect() as conn:
+#     result = conn.execute(text("select * from games_banenz.gameslist"))
 
-result_dicts = []
-for row in result.all():
-    result_dicts.append(dict(row))
+# key_dicts = result.keys()
 
-print(result_dicts) 
+# result_dicts = []
+# for row in result.all():
+#     result_dicts.append(dict(zip(key_dicts, row)))
+# print(result_dicts)
+# print(type(result_all))
+# for row in result_all:
 
+
+#   print(dict(row))
+# print(dict(row))
+
+# result_dicts.append(dict(row))
+# first_result_dicts = dict(str(result_all[]))
+# print(result_dicts)
+# print(result_all)
